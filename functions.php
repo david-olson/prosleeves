f1578cb1c1d422430f7cf2dcf5eb2c679f0d8c5e
David Olson <david.g.olson.91@gmail.com>
2018-08-08 21:40:26 -0400

    Add latest changes

--- functions.php
+++ functions.php
@@ -102,7 +102,7 @@ add_action( 'after_setup_theme', 'prosleeves_content_width', 0 );
 if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 25);
 function my_jquery_enqueue() {
    wp_deregister_script('jquery');
-   wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js", false, null);
+   wp_register_script('jquery', "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js", false, null);
    wp_enqueue_script('jquery');
 }
 
